e0fa9b9f234d10af1499cb8abc319c5d0a1e5f72
manntalati <mann.talati@gmail.com>
2025-04-13 14:34:38 -0500

    changes

---
+++ src/Navbar.tsx
@@ -0,0 +1,13 @@
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <>
+            <div>
+                <p>About</p>
+            </div>
+        </>
+    )
+}
+
+export default Navbar;
