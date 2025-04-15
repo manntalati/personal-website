d0422b892afdf3f008bf0808daefbd8ef4fbd893
manntalati <mann.talati@gmail.com>
2025-04-14 22:50:49 -0500

    changes to website

--- src/Resume.tsx
+++ src/Resume.tsx
@@ -1,10 +1,20 @@
 import './Resume.css'
+import { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import pdf from '../src/assets/talati_mann_resume.pdf'
+
+pdfjs.GlobalWorkerOptions.workerSrc = `${import.meta.env.BASE_URL}pdf.worker.min.js`;
 
 function Resume() {
     return (
         <>
             <div>   
                 <p className="heading">Resume</p>
+                <Document file='/talati_mann_resume.pdf'
+                onLoadError={console.error}
+                onSourceError={console.error}>
+                    <Page pageNumber={1}></Page>
+                </Document>
             </div>
         </>
     )
