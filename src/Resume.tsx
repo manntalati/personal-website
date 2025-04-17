c312235bc990dc1593463b23ffbbc518c66c6d0e
manntalati <mann.talati@gmail.com>
2025-04-16 23:48:04 -0500

    rough version of resume section

--- src/Resume.tsx
+++ src/Resume.tsx
@@ -1,18 +1,12 @@
 import './Resume.css'
-import { Document, Page, pdfjs } from 'react-pdf';
-
-pdfjs.GlobalWorkerOptions.workerSrc = `${import.meta.env.BASE_URL}pdf.worker.min.js`;
 
 function Resume() {
+
     return (
         <>
             <div>   
                 <p className="heading">Resume</p>
-                <Document file='/talati_mann_resume.pdf'
-                onLoadError={console.error}
-                onSourceError={console.error}>
-                    <Page pageNumber={1}></Page>
-                </Document>
+                <iframe className="pdf" src='../src/assets/talati_mann_resume.pdf' width={300} height={400}/>
             </div>
         </>
     )
