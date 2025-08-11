a5a72c58b5487155da0e9c789a8825a740a18820
Mann Talati <mann.talati@gmail.com>
2025-08-11 18:19:11 -0500

    update projects

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -73,7 +73,7 @@ export default function Projects() {
         imageUrl: "/recycling.HEIC",
         summary: "Developing an AI recycling assistant featuring a custom CNN and deployment on AWS SageMaker to classify waste materials with ≥ 70% target accuracy",
         githubUrl: "https://github.com/manntalati/ai_recycling_assistant",
-        year: "Jun 2025-Present",
+        year: "Jun 2025-Aug 2025",
     },
     {
         id: "4",
